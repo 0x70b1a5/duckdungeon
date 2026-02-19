744e5fb3b5e1c89d65d3872396a0be9dfbc4bf8c
Tobias Merkle <tobias@branchmessenger.com>
2026-02-19 14:12:06 -0500

    healing

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -18,13 +20,16 @@ pub fn player_input(
             _ => Point::zero(),
         };
 
+        let mut did_something = false;
+
+        let mut players =
+            <(Entity, &Point)>::query().filter(component::<Player>());
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
         if delta.x != 0 || delta.y != 0 {
-            let mut players =
-                <(Entity, &Point)>::query().filter(component::<Player>());
-            let (player_entity, destination) = players
-                .iter(ecs)
-                .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-                .unwrap();
 
             let mut enemies =
                 <(Entity, &Point)>::query().filter(component::<Enemy>());
@@ -36,6 +41,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -46,6 +52,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -53,6 +60,17 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+
+        }
+
+        if !did_something {
+            if let Ok(health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
 
