4b0e53226260103af0c98a65497aa3ec0f9da92d
Tobias Merkle <tobias@branchmessenger.com>
2026-02-24 19:55:21 -0500

    hud

--- src/spawner.rs
+++ src/spawner.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push((
-        Player,
+        Player{map_level: 0},
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
@@ -60,4 +60,45 @@ pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
         },
         Name("Duckington Amulet".to_string()),
     ));
+}
+
+pub fn spawn_magic_mapper(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (Item,
+         pos,
+         Render{
+             color: ColorPair::new(WHITE, BLACK),
+             glyph: to_cp437('{'),
+         },
+         Name("Dungeon Map".to_string()),
+         ProvidesDungeonMap{},
+        )
+    );
+}
+
+pub fn spawn_healing_potion(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (Item,
+         pos,
+         Render{
+             color: ColorPair::new(WHITE, BLACK),
+             glyph: to_cp437('!'),
+         },
+         Name("Healing Potion".to_string()),
+         ProvidesHealing{amount: 6},
+        )
+    );
+}
+
+pub fn spawn_entity(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    let roll = rng.roll_dice(1, 6);
+    match roll {
+        1 => spawn_healing_potion(ecs, pos),
+        2 => spawn_magic_mapper(ecs, pos),
+        _ => spawn_monster(ecs, rng, pos),
+    }
 }
